c3b9fc8f36fe270b81641a8f4393b3bddd0d4240
Hart102 <hartongit102@gmail.com>
2025-01-16 11:37:13 -0600

    worked on the header

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -4,6 +4,9 @@ import { FaTimes } from "react-icons/fa"
 import Motion from "../../component/Motion"
 
 
+import { UserIcon, StarIcon, ShoppingCartIcon, SunIcon, MoonIcon, Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
+
+
 const Navbar = () => {
 
   const [transition, setTransition] = useState("-translate-x-full");
@@ -13,7 +16,7 @@ const Navbar = () => {
   const handleClose = () => setTransition("-translate-x-full");
 
   return (
-    <nav className="w-full relative md:mb-5">
+    <nav className="w-full relative md:mb-5 [&_svg]:size-8 [&_svg]:hover:cursor-pointer">
       <div className="bg-cl-yellow-light font-bold md:text-[1.2rem] tracking-[0.1rem] uppercase text-center p-3">
         Free shipping on all U.S. orders $50+
       </div>
@@ -39,10 +42,43 @@ const Navbar = () => {
           <div className="flex-1 text-center">
             <h2 className="font-bold text-[2.7rem]">GLOWING</h2>
           </div>
-          <div className="flex items-center justify-end gap-8 flex-1">
-            <BiUser size={25} className="text-primary-color" />
-            <BiStar size={25} className="text-primary-color" />
-            <BiCart size={25} className="text-primary-color" />
+          <div className="flex items-center justify-end gap-10 flex-1">
+            <UserIcon />
+            <div className="relative">
+              <StarIcon className="font-extralight" />
+              <div
+                className="
+                flex 
+                items-center 
+                justify-center 
+                absolute -top-2 -right-3 
+                w-6 h-6 p-1 
+                rounded-full 
+                text-white bg-cl-black"
+              >2</div>
+            </div>
+            <div className="relative">
+              <ShoppingCartIcon />
+              <div
+                className="
+                flex 
+                items-center 
+                justify-center 
+                absolute -top-2 -right-3 
+                w-6 h-6 p-1 
+                rounded-full 
+                text-white bg-cl-black"
+              >2</div>
+            </div>
+
+            <div className="flex items-center gap-6 px-3 py-1 shadow-xl rounded-full [&_div]:rounded-full [&_div]:p-1">
+              <div className="bg-[#BEBE57]">
+                <SunIcon />
+              </div>
+              <div className="bg-cl-black text-white">
+                <MoonIcon />
+              </div>
+            </div>
           </div>
         </div>
 
@@ -64,7 +100,7 @@ const Navbar = () => {
 
       {/* Mobile View */}
       <div className="grid grid-cols-3 items-center p-4 md:hidden">
-        <div><BiMenu onClick={handleOpen} size={25} className="text-primary-color" /></div>
+        <div><Bars3Icon onClick={handleOpen} size={25} /></div>
         <div className="text-center">
           <h2 className="font-bold text-2xl">GLOWING</h2>
         </div>
@@ -73,7 +109,7 @@ const Navbar = () => {
       <div className={`flex flex-col gap-8 p-4 absolute top-0 w-full h-[100vh] bg-white ${transition} delay-100 duration-150 md:hidden`}>
         <div className="flex items-center justify-between">
           <h2 className="font-bold text-2xl">GLOWING</h2>
-          <FaTimes onClick={handleClose} size={25} className="text-primary-color" />
+          <XMarkIcon onClick={handleClose} size={25} className="text-primary-color" />
         </div>
         <div className="flex flex-col gap-4 uppercase font-semibold">
           {
